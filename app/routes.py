5c48393fc01d41686812488fc29a13cecc3d4103
MaxSush <psushant880@gmail.com>
2024-12-04 02:36:25 +0530

    home page initialize

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 import atexit
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template, jsonify, request
 
 from app.getpackets import GetPacket, get_interface
 
@@ -7,9 +7,9 @@ main = Blueprint('main', __name__)
 
 obj = []
 interfaces = get_interface() 
-print(interfaces)
 for i in interfaces:
     obj.append(GetPacket(interface=i))
+interfaces.append("any")
 
 @main.route('/')
 def index():
@@ -18,10 +18,23 @@ def index():
 @main.route('/data')
 def data():
     results = []
+    total_packets = 0
     for o in obj:
-        results.append(o.get_packets_sum())
+        packets = o.get_packets_sum()
+        total_packets += packets
+        results.append(packets)
         o.reset_result()
+    results.append(total_packets)
     return jsonify({'interfaces': interfaces, 'value': results})
+
+@main.route('/interface/<interface_name>', methods=['GET'])
+def interface_page(interface_name):
+    filters = request.args.get('filters', 'None')
+    return f"Showing details for interface {interface_name} with filters: {filters}"
+
+@main.route('/new')
+def new():
+    return render_template('new.html')
 
 @atexit.register
 def cleanup():
